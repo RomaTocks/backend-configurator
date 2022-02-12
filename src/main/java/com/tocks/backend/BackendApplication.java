db0d164b1449e4d4cc70366257babbe02bca3398
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-02-13 02:27:38 +0300

    Init project.

---
+++ src/main/java/com/tocks/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.tocks.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.tocks.backend.repository")
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
