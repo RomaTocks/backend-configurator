7ef810360337a94fe5e209ce0575be4efeb537b7
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-02-19 04:10:47 +0300

    Update searching of base package for repository layer.

--- src/main/java/com/tocks/backend/BackendApplication.java
+++ src/main/java/com/tocks/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.tocks.backend.repository")
+@EnableMongoRepositories(basePackages = "com.tocks.backend.repository.*")
 public class BackendApplication {
 
 	public static void main(String[] args) {
