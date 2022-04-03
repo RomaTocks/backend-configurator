0897cd0ebd82936fb686f4e4242ec59c3f5a6b74
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-04 00:26:39 +0300

    Update product's model.

--- src/main/java/com/tocks/backend/model/product/Product.java
+++ src/main/java/com/tocks/backend/model/product/Product.java
@@ -1,12 +1,15 @@
 package com.tocks.backend.model.product;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.tocks.backend.model.common.Manufacturer;
-import com.tocks.backend.model.common.Prices;
+import com.tocks.backend.model.common.additional.Images;
+import com.tocks.backend.model.common.additional.Manufacturer;
+import com.tocks.backend.model.common.additional.Rating;
 import com.tocks.backend.model.common.sellers.Position;
+import com.tocks.backend.model.common.sellers.Prices;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -21,9 +24,23 @@ public class Product {
     protected Rating reviews;
     protected Manufacturer manufacturer;
     protected String description;
-    protected Manufacturer manufacturer;
     protected Prices prices;
     protected String html_url;
     protected List<Position> positions;
     private Integer total;
+
+    public Object getFieldValue(String name) {
+        Field field = null;
+        try
+        {
+            field = this.getClass().getDeclaredField(name);
+            return field.get(this);
+        }
+        catch (NoSuchFieldException | IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
