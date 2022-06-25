c670b98fe05ccad55a3956b59b43462273fab993
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-06-25 03:46:36 +0300

    Update jwt structure.

--- src/main/java/com/tocks/backend/configuration/jwt/JwtFilter.java
+++ src/main/java/com/tocks/backend/configuration/jwt/JwtFilter.java
@@ -15,13 +15,9 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-import static org.springframework.util.StringUtils.hasText;
-
 @Component
 public class JwtFilter extends GenericFilterBean
 {
-
-    public static final String AUTHORIZATION = "Authorization";
 
     @Autowired
     private JwtProvider jwtProvider;
@@ -32,7 +28,7 @@ public class JwtFilter extends GenericFilterBean
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException
     {
-        String token = getTokenFromRequest((HttpServletRequest) servletRequest);
+        String token = jwtProvider.getTokenFromRequest((HttpServletRequest) servletRequest);
         if (token != null && jwtProvider.validateToken(token)) {
             String userLogin = jwtProvider.getLoginFromToken(token);
             CustomUserDetails customUserDetails = customUserDetailsService.loadUserByUsername(userLogin);
@@ -40,13 +36,5 @@ public class JwtFilter extends GenericFilterBean
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
         filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    private String getTokenFromRequest(HttpServletRequest request) {
-        String bearer = request.getHeader(AUTHORIZATION);
-        if (hasText(bearer) && bearer.startsWith("Bearer ")) {
-            return bearer.substring(7);
-        }
-        return null;
     }
 }
--- src/main/java/com/tocks/backend/configuration/jwt/JwtProvider.java
+++ src/main/java/com/tocks/backend/configuration/jwt/JwtProvider.java
@@ -1,15 +1,16 @@
 package com.tocks.backend.configuration.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @Component
 @Slf4j
@@ -17,6 +18,8 @@ public class JwtProvider {
 
     @Value("$(jwt.secret)")
     private String jwtSecret;
+
+    public static final String AUTHORIZATION = "Authorization";
 
     public String generateToken(String login) {
         Date date = Date.from(LocalDate.now().plusDays(15).atStartOfDay(ZoneId.systemDefault()).toInstant());
@@ -31,14 +34,46 @@ public class JwtProvider {
         try {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
             return true;
+        } catch (ExpiredJwtException expEx) {
+            log.warn("Token expired");
+        } catch (UnsupportedJwtException unsEx) {
+            log.warn("Unsupported jwt");
+        } catch (MalformedJwtException mjEx) {
+            log.warn("Malformed jwt");
+        } catch (SignatureException sEx) {
+            log.warn("Invalid signature");
         } catch (Exception e) {
-            log.error("Received invalid token.");
+            log.warn("invalid token");
         }
         return false;
     }
 
+    public String getTokenFromRequest(HttpServletRequest request) {
+        String bearer = request.getHeader(AUTHORIZATION);
+        return getToken(bearer);
+    }
+    public String getToken(String bearerToken) {
+        if (hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
     public String getLoginFromToken(String token) {
-        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-        return claims.getSubject();
+        try {
+            Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+            return claims.getSubject();
+        } catch (ExpiredJwtException expEx) {
+            log.warn("Token expired");
+        } catch (UnsupportedJwtException unsEx) {
+            log.warn("Unsupported jwt");
+        } catch (MalformedJwtException mjEx) {
+            log.warn("Malformed jwt");
+        } catch (SignatureException sEx) {
+            log.warn("Invalid signature");
+        } catch (Exception e) {
+            log.warn("invalid token");
+        }
+        return null;
     }
 }
