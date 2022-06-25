d8005ddec04c4a278328426a29748b797249441c
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-06-25 03:59:08 +0300

    Update configuration controller.

--- src/main/java/com/tocks/backend/controller/configuration/ConfigurationController.java
+++ src/main/java/com/tocks/backend/controller/configuration/ConfigurationController.java
@@ -1,14 +1,17 @@
 package com.tocks.backend.controller.configuration;
 
+import com.tocks.backend.dto.configuration.CheckedConfigurationResponse;
+import com.tocks.backend.dto.configuration.UserConfigurationsRequest;
+import com.tocks.backend.dto.configuration.UserConfigurationsResponse;
+import com.tocks.backend.dto.favourite.FavouriteProductsResponse;
 import com.tocks.backend.model.configuration.Configuration;
+import com.tocks.backend.model.product.FavouriteProducts;
 import com.tocks.backend.service.implementation.ConfigurationServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 @RestController
-@RequestMapping("/config")
+@RequestMapping()
 public class ConfigurationController
 {
     private final ConfigurationServiceImpl service;
@@ -18,10 +21,17 @@ public class ConfigurationController
         this.service = service;
     }
 
-    @PostMapping()
+    @PostMapping("/config")
     @CrossOrigin(origins = "*")
-    public ResponseEntity<Map<String,Object>> config(@RequestBody Configuration configuration) {
-        System.out.println(configuration);
+    public ResponseEntity<CheckedConfigurationResponse> configureComponentsOfPersonalComputer(@RequestBody Configuration configuration) {
         return service.checkConfiguration(configuration);
+    }
+    @GetMapping("api/user/favourite")
+    public FavouriteProductsResponse getFavouriteProducts(@RequestBody FavouriteProducts favouriteProducts) {
+        return service.findAllFavouriteProducts(favouriteProducts);
+    }
+    @GetMapping("/api/user/configuration")
+    public ResponseEntity<UserConfigurationsResponse> getUserConfigurations(@RequestBody UserConfigurationsRequest userConfigurationsRequest) {
+        return service.getUserConfigurations(userConfigurationsRequest);
     }
 }
